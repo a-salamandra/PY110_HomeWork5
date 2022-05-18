a6965cdb702d4ed1f79ac92ffb5ad85791420f4b
a-salamandra <la.februaro@gmail.com>
2022-05-18 20:15:39 +0300

    added task 1

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-
+import tasks.task1 as task1
 if __name__ == '__main__':
-    pass
+
+    list1 = [0, 1, 1, 0]
+    list2 = [1, 1, 1, 1]
+
+    print(task1.task1(list1))
+    print(task1.task1(list2))
--- tasks/task1.py
+++ tasks/task1.py
@@ -1,2 +1,3 @@
-def task1():
-    pass
+def task1(binary_number: list) -> int:
+    str_ = "".join([str(i) for i in binary_number])
+    return int(str_,base=2)
